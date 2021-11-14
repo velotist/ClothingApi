71e411a15ea3ddc3ce14866ce0bd56369d1ac4df
velotist <velospam@posteo.de>
2021-11-14 16:57:26 +0100

    Add Keycloak authorization
    [CLAPI-1]

---
+++ Infrastructure/Keycloak/Requirement.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace ClothingApi.Infrastructure.Keycloak
+{
+    public class Requirement : IAuthorizationRequirement
+    {
+        public Requirement(string policyName)
+        {
+            PolicyName = policyName;
+        }
+
+        public string PolicyName { get; }
+    }
+}
