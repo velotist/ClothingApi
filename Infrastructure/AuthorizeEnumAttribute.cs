79e0d4c212d458a88fa84ff1fc78bcf77303470f
velotist <velospam@posteo.de>
2021-11-14 14:28:33 +0100

    Add roles for authorizing
    [CLAPI-1]

---
+++ Infrastructure/AuthorizeEnumAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ClothingApi.Infrastructure
+{
+    public class AuthorizeEnumAttribute : AuthorizeAttribute
+    {
+        public AuthorizeEnumAttribute(params object[] roles)
+        {
+            if (roles.Any(role => role.GetType().BaseType != typeof(Enum)))
+            {
+                throw new ArgumentException(null, nameof(roles));
+            }
+
+            Roles = string.Join(",",
+                roles.Select(role => Enum.GetName(role.GetType(), role)));
+        }
+    }
+}
---
+++ Models/Roles.cs
@@ -0,0 +1,9 @@
+﻿namespace ClothingApi.Models
+{
+    public enum Roles
+    {
+        View = 1,
+        Editor = 2,
+        Manager = 3
+    }
+}
